6a8c16c7df13c6e8b5f07cdc2e9fa836895df8ac
Vakantanka <laszlo.fenyvesi@gmail.com>
2021-12-29 06:52:50 +0100

    svg kiválasztás ok

--- script.js
+++ script.js
@@ -13,7 +13,27 @@ async function loadEvent() {
     // const arr = await res.json();
 
     // document.getElementById("root").insertAdjacentHTML("beforeend",renderPage(arr));
-    document.getElementById("root").insertAdjacentHTML("beforeend",`<h1>Load completed</h1>`);
+    document.getElementById("root").insertAdjacentHTML("beforeend",`<div id="svgContainer"></div>`);
+
+    xhr = new XMLHttpRequest();
+    xhr.open("GET","small_restaurant_optimized.svg",false);
+    xhr.overrideMimeType("image/svg+xml");
+    xhr.onload = function(e) {
+        document.getElementById("svgContainer").appendChild(xhr.responseXML.documentElement);
+    };
+    xhr.send("");
+
+    const tableList = document.querySelectorAll(`[id^='table']`);
+    for (let table of tableList) {
+        table.classList.add("table");
+        table.addEventListener("click",function(evt) {
+            console.log(table.id);
+            table.classList.add("red");
+        },
+        false);
+        console.log(table);
+    }    
+
     console.log("Load completed.");
 }
 
