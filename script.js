40d92663a7202544025107c7e264a89dc4b300b4
Vakantanka <laszlo.fenyvesi@gmail.com>
2021-12-30 12:52:37 +0100

    Foglalás form fent van

--- script.js
+++ script.js
@@ -1,15 +1,76 @@
-const renderPage = (arr) => {
-    let returnHTML = "";
+const timeZoneList = [
+    {
+        name: "morning",
+        timeStart: "06:00",
+        timeEnd: "08:30"
+    },
+    {
+        name: "forenoon",
+        timeStart: "09:00",
+        timeEnd: "10:30"
+    },
+    {
+        name: "lunch",
+        timeStart: "11:00",
+        timeEnd: "13:30"
+    },
+    {
+        name: "afternoon",
+        timeStart: "14:00",
+        timeEnd: "16:30"
+    },
+    {
+        name: "dinner",
+        timeStart: "17:00",
+        timeEnd: "21:30"
+    },
+    {
+        name: "night",
+        timeStart: "22:00",
+        timeEnd: "23:30"
+    }
+]
 
-    for (const item of arr) {
-        returnHTML += ``;
+const renderPage = () => {
+    
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    if (month < 10) month = "0" + month;
+    if (day < 10) day = "0" + day;
+    let today = year + "-" + month + "-" + day;       
+        
+    let timeZoneOptions = '';
+    for (const timeZone of timeZoneList) {
+        timeZoneOptions += `<option value="${timeZone.timeStart} - ${timeZone.timeEnd}">${timeZone.name}: ${timeZone.timeStart} - ${timeZone.timeEnd}</option>`;
     }
 
+    let returnHTML = `
+    <h1>Senveday restaurant</h1>
+    <h2>Coalb us & can year</h2>
+    <h3 class="slogan"><q>It hits the ground or reaches the sky</q></h3>
+    <div id="reservation">
+        <form> 
+            <h3>Reservation</h3>
+            <label for="date">Date</label>
+            <input type="date" name="date" value="${today}">
+            <label for="timeZone">Time zone</label>
+            <select name="timeZone">${timeZoneOptions}</select>
+            <label for="time">Reservation length in hour</label>
+            <input type="number" name="time" value="1" min="1" max="6">
+        </form>
+        <div id="svgContainer">
+            <h3>Pick a table</h3>
+        </div>
+        <div id="">
+    </div>
+    `;
     return returnHTML;
 }
 
 async function loadEvent() {
-    document.getElementById("root").insertAdjacentHTML("beforeend",`<div id="svgContainer"></div>`);
+    document.getElementById("root").insertAdjacentHTML("beforeend",renderPage());
     fetch('./small_restaurant_optimized.svg')
     .then(function (response) {
         return response.text();
@@ -21,9 +82,7 @@ async function loadEvent() {
             table.classList.add("table");
             table.addEventListener("click",function(evt) {
                 console.log(table.id);
-                table.classList.add("red");
             }, false);
-//            console.log(table);
         }    
     })
     .catch(function (err) {
